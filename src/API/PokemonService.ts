15bb5414064390bf9bb62e6d9fa5032ea32e1b4e
Dabiny <lina917@naver.com>
2023-01-05 13:24:26 +0900

    특정포켓몬 특수문자 제거

--- src/API/PokemonService.ts
+++ src/API/PokemonService.ts
@@ -163,7 +163,8 @@ export const searchKoreanAPI = async () => {
         const nameData = await res.then((response) =>
             response.data.names.map((v) => v.name)
         );
-        mp.set(nameData[2].replace(/[가-힣]/g), nameData[7].toLowerCase());
+        const replaceName = nameData[2].replace(/[^가-힣]/g, "");
+        mp.set(replaceName, nameData[7].toLowerCase());
     }
     return mp;
 
